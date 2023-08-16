6ceef0752fc07293f75254966352f6481a99c10d
eodaneze <ezealiokechukwu1999@gmail.com>
2023-08-16 12:13:17 -0700

    creating a new product and storing it in db

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,16 +1,40 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const CreatePage = () => {
   const[name, setName] = useState("")
   const[price, setPrice] = useState("")
   const[quantity, setQuantity] = useState("")
-  const[image, setImage] = useState("")
+  const[image, setImage] = useState("");
+  const[isLoading, setIsLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const saveProduct = async(e) => {
+    e.preventDefault();
+    // alert(name)
+
+    if(name === "" || quantity === "" || price === "" || image === ""){
+       alert("Please fill out all the input completely");
+    } 
+    try{
+        setIsLoading(true);
+      const response = await axios.post("http://localhost:3000/api/products", {name: name, quantity: quantity, price: price, image: image});
+      alert(`Save ${response.data.name} Successfully`);
+      setIsLoading(false)
+      navigate('/');
+    }catch(err){
+      console.log(err);
+      isLoading(false)
+    }
+  }
   return (
     <div className='max-w-lg bg-white shadow-lg mx-auto p-7 rounded mt-6'>
         <h2 className='font-semibold text-2xl mb-4 block text-center'>
           Create a Product
         </h2>
-        <form>
+        <form onSubmit={saveProduct}>
             <div className='space-y-2'>
                 <div>
                    <label>Name</label>
@@ -22,14 +46,14 @@ const CreatePage = () => {
                 </div>
                 <div>
                    <label>Price</label>
-                   <input type="number"  value={price} onChange={(e) => setQuantity(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Price' />
+                   <input type="number"  value={price} onChange={(e) => setPrice(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Price' />
                 </div>
                 <div>
                    <label>Image Url</label>
                    <input type="text"  value={image} onChange={(e) => setImage(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Image Url' />
                 </div>
                 <div>
-                    <button className='block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer'>Save</button>
+                    {!isLoading && (<button className='block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer'>Save</button>)}
                 </div>
             </div>
         </form>
