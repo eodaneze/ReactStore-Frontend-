169de9ded5a29cb7b222dc65953a646e7afed622
eodaneze <ezealiokechukwu1999@gmail.com>
2023-08-16 11:04:16 -0700

    designing the create product page

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreatePage = () => {
+  const[name, setName] = useState("")
+  const[price, setPrice] = useState("")
+  const[quantity, setQuantity] = useState("")
+  const[image, setImage] = useState("")
   return (
-    <div>CreatePage</div>
+    <div className='max-w-lg bg-white shadow-lg mx-auto p-7 rounded mt-6'>
+        <h2 className='font-semibold text-2xl mb-4 block text-center'>
+          Create a Product
+        </h2>
+        <form>
+            <div className='space-y-2'>
+                <div>
+                   <label>Name</label>
+                   <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Product name' />
+                </div>
+                <div>
+                   <label>Quantity</label>
+                   <input type="number"  value={quantity} onChange={(e) => setQuantity(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Quantity' />
+                </div>
+                <div>
+                   <label>Price</label>
+                   <input type="number"  value={price} onChange={(e) => setQuantity(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Price' />
+                </div>
+                <div>
+                   <label>Image Url</label>
+                   <input type="text"  value={image} onChange={(e) => setImage(e.target.value)} className='w-full block border p-3 text-gray-600 rounded focus:outline-none focus:shadow-outline focus:border-blue-200 placeholder:gray-400' placeholder='Enter Image Url' />
+                </div>
+                <div>
+                    <button className='block w-full mt-6 bg-blue-700 text-white rounded-sm px-4 py-2 font-bold hover:bg-blue-600 hover:cursor-pointer'>Save</button>
+                </div>
+            </div>
+        </form>
+    </div>
   )
 }
 
