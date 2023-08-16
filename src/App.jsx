55e1060eca13f386e66df89092d93afbf0cc7f98
eodaneze <ezealiokechukwu1999@gmail.com>
2023-08-16 09:46:08 -0700

    making a call to the backend api using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
+import EditPage from './pages/EditPage'
 
 const App = () => {
   return (
-    <Routes>
-       <Route index element={<HomePage />} />
-      <Route path='/create' element={<CreatePage />} />
-       
-    </Routes>
+    <div>
+      <nav className="bg-gray-800">
+          <div className="container mx-auto p-2">
+              <Link to={'/'}>
+                  <h2 className="text-white text-2xl font-bold">React Crud</h2> 
+              </Link>
+          </div>
+      </nav>
+       <Routes>
+          <Route index element={<HomePage />} />
+          <Route path='/create' element={<CreatePage />} />
+          <Route path='/edit' element={<EditPage />} />   
+      </Routes>
+    </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
+    const[products, setProducts] = useState([]);
+    const[isLoading, setIsLoading] = useState(false);
+const getProducts = async() => {
+    try{
+     setIsLoading(true);
+     const response = await axios.get("http://localhost:3000/api");
+     console.log(response.data);
+     setProducts(response.data);
+    }catch(err){
+       console.log(err);
+    }
+}
+    useEffect(() => {
+        
+        getProducts()
+    }, [])
   return (
     <div>This is the homepage of the application</div>
   )
